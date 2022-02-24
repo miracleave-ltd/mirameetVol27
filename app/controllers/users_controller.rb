e333b37fdeabba1590f9915e1a912b04f2418802
ryota-1231 <rrrrr.1231@gmail.com>
2022-02-24 19:21:08 +0900

    微修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @posts   = user.posts.order("created_at DESC").page(params[:page]).per(5)
+    @posts = user.posts.order("created_at DESC").page(params[:page]).per(5)
   end
 end
