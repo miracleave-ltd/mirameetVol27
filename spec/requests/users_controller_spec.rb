e9a5123b29be00fd0a2a01f07ebf55a6dba1afb2
ryota-1231 <rrrrr.1231@gmail.com>
2022-02-27 21:35:13 +0900

    ファイル名修正

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'UsersControllers', type: :request do
+RSpec.describe 'UsersController', type: :request do
   describe 'GET #show' do
     context 'ユーザーが存在する場合' do
       let(:user) { create(:user, nickname: 'Takashi') }
