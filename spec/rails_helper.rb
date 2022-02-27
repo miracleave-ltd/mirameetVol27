38c0d68c873a1c7dfed5026b5da15cae08d70291
naomi-0913 <upworld409019@gmail.com>
2022-02-27 16:57:37 +0900

    add rspec_config (非推奨警告をエラーにする)

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,5 +62,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.raise_errors_for_deprecations!
+
   config.include FactoryBot::Syntax::Methods
 end
