b0d5f3d3fdbb60e78c412ee921237f166fa7d97b
ryota-1231 <rrrrr.1231@gmail.com>
2022-03-01 18:46:21 +0900

    rspecのジェネレータを使用してファイルを再作成。exampleの文言を修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe User do
+RSpec.describe User, type: :model do
   let(:user) { build(:user) }
   subject { user.save }
 
@@ -12,27 +12,27 @@ describe User do
     end
 
     context 'nickname' do
-      it 'nilの場合、保存できない' do
+      it 'nilの場合、保存できないこと' do
         user.nickname = nil
         is_expected.to eq(false)
       end
 
-      it '空文字の場合、保存できない' do
+      it '空文字の場合、保存できないこと' do
         user.nickname = ""
         is_expected.to eq(false)
       end
 
-      it '10文字以内の場合、保存できる' do
+      it '10文字以内の場合、保存できること' do
         user.nickname = Faker::Lorem.unique.characters(number: 10)
         is_expected.to eq(true)
       end
 
-      it '11文字以上の場合、保存できない' do
+      it '11文字以上の場合、保存できないこと' do
         user.nickname = Faker::Lorem.unique.characters(number: 11)
         is_expected.to eq(false)
       end
 
-      it 'すでに使用されているnicknameの場合、保存できない' do
+      it 'すでに使用されているnicknameの場合、保存できないこと' do
         subject
         next_user = build(:user, nickname: user.nickname)
         expect(next_user.save).to eq(false)
@@ -40,27 +40,27 @@ describe User do
     end
 
     context 'email' do
-      it 'nilの場合、保存できない' do
+      it 'nilの場合、保存できないこと' do
         user.email = nil
         is_expected.to eq(false)
       end
 
-      it '空文字の場合、保存できない' do
+      it '空文字の場合、保存できないこと' do
         user.email = ""
         is_expected.to eq(false)
       end
 
-      it 'emailの形式ではない場合、保存できない' do
+      it 'emailの形式ではない場合、保存できないこと' do
         user.email = Faker::Lorem.sentence
         is_expected.to eq(false)
       end
 
-      it '全角文字を使用する場合、保存できない' do
+      it '全角文字を使用する場合、保存できないこと' do
         user.email = 'ｅｘａｐｌｅ@gmail.com'
         is_expected.to eq(false)
       end
 
-      it 'すでに使用されているemailの場合、保存できない' do
+      it 'すでに使用されているemailの場合、保存できないこと' do
         subject
         next_user = build(:user, email: user.email)
         expect(next_user.save).to eq(false)
@@ -68,45 +68,45 @@ describe User do
     end
 
     context 'password' do
-      it 'passwordがnilの場合、保存できない' do
+      it 'passwordがnilの場合、保存できないこと' do
         user.password = nil
         is_expected.to eq(false)
       end
 
-      it 'passwordが空文字の場合、保存できない' do
+      it 'passwordが空文字の場合、保存できないこと' do
         user.password = ""
         is_expected.to eq(false)
       end
 
-      it 'password_confirmationがnilの場合、保存できない' do
+      it 'password_confirmationがnilの場合、保存できないこと' do
         user.password_confirmation = nil
         is_expected.to eq(false)
       end
 
-      it 'password_confirmationが空文字の場合、保存できない' do
+      it 'password_confirmationが空文字の場合、保存できないこと' do
         user.password_confirmation = ""
         is_expected.to eq(false)
       end
 
-      it 'passwordとpassword_confirmationが不一致の場合、保存できない' do
+      it 'passwordとpassword_confirmationが不一致の場合、保存できないこと' do
         user.password = 'password'
         user.password_confirmation = 'password_confirmation'
         is_expected.to eq(false)
       end
 
-      it '5文字以下の場合、保存できない' do
+      it '5文字以下の場合、保存できないこと' do
         user.password = user.password_confirmation = Faker::Lorem.characters(number: 5)
         is_expected.to eq(false)
       end
 
-      it '6文字以上、128文字以下の場合、保存できる' do
+      it '6文字以上、128文字以下の場合、保存できること' do
         user.password = user.password_confirmation = Faker::Lorem.characters(number: 6)
         is_expected.to eq(true)
         user.password = user.password_confirmation = Faker::Lorem.characters(number: 128)
         is_expected.to eq(true)
       end
 
-      it '129文字以上の場合、保存できない' do
+      it '129文字以上の場合、保存できないこと' do
         user.password = user.password_confirmation = Faker::Lorem.characters(number: 129)
         is_expected.to eq(false)
       end
