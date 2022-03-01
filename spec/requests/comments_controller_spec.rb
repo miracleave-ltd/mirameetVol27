3e2c2e09b71570cd7d2f1d3090d159f7c974e548
ryota-1231 <rrrrr.1231@gmail.com>
2022-03-01 18:55:50 +0900

    ログインしていない場合、302レスポンスを返すことのテストケースを追加

--- spec/requests/comments_controller_spec.rb
+++ spec/requests/comments_controller_spec.rb
@@ -62,6 +62,11 @@ RSpec.describe 'CommentsController', type: :request do
         sign_out user
       end
 
+      it '302レンスポンスを返すこと' do
+        subject
+        expect(response.status).to eq 302
+      end
+
       it 'ログイン画面にリダイレクトされること' do
         subject
         expect(response).to redirect_to new_user_session_url
