29e3ba30a90302bca618db980b4e9762e91415c7
ryota-1231 <rrrrr.1231@gmail.com>
2022-03-01 18:43:57 +0900

    ファクトリーボットのファイル名の変更とアソシエーションの追加

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :comment do
     text {Faker::Lorem.sentence}
+    association :post
+    user { post.user }
 
     trait :text_invalid do
       text {nil}
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :post do
+    image {Faker::Internet.url}
     text {Faker::Lorem.sentence}
+    association :user
 
     trait :image_present do
       image {Faker::Internet.url}
