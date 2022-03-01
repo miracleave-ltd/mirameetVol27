daca5b6bd706f866dd74da66c761d266d38331b2
ryota-1231 <rrrrr.1231@gmail.com>
2022-03-01 21:38:14 +0900

    テストの追加

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe 'UsersController', type: :request do
   describe 'GET #show' do
     context 'ユーザーが存在する場合' do
       let(:user) { create(:user, nickname: 'Takashi') }
+      let!(:post) { create(:post, text: 'PostRequestTest', user: user)}
+      subject { get user_path user.id }
 
       context 'ログインしている場合' do
         before do
@@ -11,13 +13,19 @@ RSpec.describe 'UsersController', type: :request do
         end
 
         it '200レスポンスを返すこと' do
-          get user_path user.id
+          subject
           expect(response.status).to eq 200
         end
 
         it 'ユーザー名が表示されること' do
-          get user_path user.id
+          subject
           expect(response.body).to include 'Takashi'
+        end
+
+        it 'レスポンスに適切な投稿内容を含むこと' do
+          subject
+          expect(response.body).to include 'PostRequestTest :Takashiの投稿'
+          expect(response.body).to include '<span>投稿者</span>Takashi'
         end
       end
 
@@ -26,17 +34,28 @@ RSpec.describe 'UsersController', type: :request do
           sign_out user
         end
 
-        it 'リクエストが成功しないこと' do
-          get user_path user.id
+        it '302レスポンスを返すこと' do
+          subject
           expect(response.status).to eq 302
+        end
+
+        it 'ログイン画面にリダイレクトされること' do
+          subject
+          expect(response).to redirect_to new_user_session_url
         end
       end
     end
 
     context 'ユーザーが存在しない場合' do
-      it 'リクエストが成功しないこと' do
-        get user_url 1
+      subject { get user_url 1 }
+      it '302レスポンスを返すこと' do
+        subject
         expect(response.status).to eq 302
+      end
+
+      it 'ログイン画面にリダイレクトされること' do
+        subject
+        expect(response).to redirect_to new_user_session_url
       end
     end
   end
