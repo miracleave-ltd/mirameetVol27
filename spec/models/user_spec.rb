059dbc0ea99c6e947a43994b486e18c93a493d25
ryota-1231 <rrrrr.1231@gmail.com>
2022-03-03 22:42:26 +0900

    Shoulda::Matchersの導入とModelテストのリファクタリング

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,115 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { build(:user) }
-  subject { user.save }
+  it "nickname, email, password, password_confirmationがあれば有効であること" do
+    user = User.new(
+      nickname: 'Takashi',
+      email: 'tester@example.com',
+      password: 'p@ssword!!',
+      password_confirmation: 'p@ssword!!',
+    )
+    expect(user).to be_valid
+  end
 
-  describe 'validationテスト' do
-    context '正常チェック' do
-      it '保存できること' do
-        is_expected.to eq(true)
-      end
+  describe 'アソシエーション' do
+    it { is_expected.to have_many :posts }
+    it { is_expected.to have_many :comments }
+  end
+
+  describe 'nickname' do
+    it { is_expected.to validate_presence_of :nickname }
+    it { is_expected.to validate_uniqueness_of :nickname }
+    it { is_expected.to validate_length_of(:nickname).is_at_most(10) }
+  end
+
+  describe 'email' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it 'emailの形式ではない場合、無効な状態であること' do
+      user = build(:user, email: 'example_no_email')
+      user.valid?
+      expect(user.errors[:email]).to include("は不正な値です") #invalid
     end
 
-    context 'nickname' do
-      it 'nilの場合、保存できないこと' do
-        user.nickname = nil
-        is_expected.to eq(false)
-      end
-
-      it '空文字の場合、保存できないこと' do
-        user.nickname = ""
-        is_expected.to eq(false)
-      end
-
-      it '10文字以内の場合、保存できること' do
-        user.nickname = Faker::Lorem.unique.characters(number: 10)
-        is_expected.to eq(true)
-      end
-
-      it '11文字以上の場合、保存できないこと' do
-        user.nickname = Faker::Lorem.unique.characters(number: 11)
-        is_expected.to eq(false)
-      end
-
-      it 'すでに使用されているnicknameの場合、保存できないこと' do
-        subject
-        next_user = build(:user, nickname: user.nickname)
-        expect(next_user.save).to eq(false)
-      end
+    it 'emailは全角文字を使用する場合、無効な状態であること' do
+      user = build(:user, email: 'ｅｘａｐｌｅ@gmail.com')
+      user.valid?
+      expect(user.errors[:email]).to include("は不正な値です") #invalid
     end
+  end
 
-    context 'email' do
-      it 'nilの場合、保存できないこと' do
-        user.email = nil
-        is_expected.to eq(false)
-      end
+  describe 'password' do
+    it { is_expected.to validate_presence_of :password }
+    it { is_expected.to validate_length_of(:password).is_at_least(6).is_at_most(128) }
+    it { is_expected.to validate_presence_of :password_confirmation }
 
-      it '空文字の場合、保存できないこと' do
-        user.email = ""
-        is_expected.to eq(false)
-      end
-
-      it 'emailの形式ではない場合、保存できないこと' do
-        user.email = Faker::Lorem.sentence
-        is_expected.to eq(false)
-      end
-
-      it '全角文字を使用する場合、保存できないこと' do
-        user.email = 'ｅｘａｐｌｅ@gmail.com'
-        is_expected.to eq(false)
-      end
-
-      it 'すでに使用されているemailの場合、保存できないこと' do
-        subject
-        next_user = build(:user, email: user.email)
-        expect(next_user.save).to eq(false)
-      end
-    end
-
-    context 'password' do
-      it 'passwordがnilの場合、保存できないこと' do
-        user.password = nil
-        is_expected.to eq(false)
-      end
-
-      it 'passwordが空文字の場合、保存できないこと' do
-        user.password = ""
-        is_expected.to eq(false)
-      end
-
-      it 'password_confirmationがnilの場合、保存できないこと' do
-        user.password_confirmation = nil
-        is_expected.to eq(false)
-      end
-
-      it 'password_confirmationが空文字の場合、保存できないこと' do
-        user.password_confirmation = ""
-        is_expected.to eq(false)
-      end
-
-      it 'passwordとpassword_confirmationが不一致の場合、保存できないこと' do
-        user.password = 'password'
-        user.password_confirmation = 'password_confirmation'
-        is_expected.to eq(false)
-      end
-
-      it '5文字以下の場合、保存できないこと' do
-        user.password = user.password_confirmation = Faker::Lorem.characters(number: 5)
-        is_expected.to eq(false)
-      end
-
-      it '6文字以上、128文字以下の場合、保存できること' do
-        user.password = user.password_confirmation = Faker::Lorem.characters(number: 6)
-        is_expected.to eq(true)
-        user.password = user.password_confirmation = Faker::Lorem.characters(number: 128)
-        is_expected.to eq(true)
-      end
-
-      it '129文字以上の場合、保存できないこと' do
-        user.password = user.password_confirmation = Faker::Lorem.characters(number: 129)
-        is_expected.to eq(false)
-      end
+    it 'passwordとpassword_confirmationが不一致の場合、無効な状態であること' do
+      user = build(:user, password: 'password', password_confirmation: 'password_confirmation')
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません") #confirmation
     end
   end
 end
