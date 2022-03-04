3cad5127bf756fea61d966714ab939eed882f523
toshihiko kobayashi <toshihiko316@gmail.com>
2022-03-04 09:32:19 +0900

    deviseのコントローラヘルパーを追記

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  describe '#showアクションテスト' do
+    context 'ユーザが存在する場合' do
+      let(:user) { create(:user, nickname: 'Toshio') }
+      let(:post_instance) { create(:post, user: user, text: 'PostTest', image: 'https://example_image_url') }
+
+      context 'ログインしている場合' do
+        before do
+          sign_in user
+          post_instance
+        end
+
+        it '200レスポンスを返すこと' do
+          get user_path user.id
+          expect(response.status).to eq 200
+        end
+
+        it '正常にレスポンスを返すこと' do
+          get user_path user.id
+          expect(response).to be_successful
+        end
+
+        it 'ユーザー名が表示していること' do
+          get user_path user.id
+          expect(user.nickname).to eq 'Toshio'          
+        end
+
+        it '投稿が表示されていること' do
+          expect(post_instance.text).to include 'PostTest'        
+        end
+      end
+
+      # context 'ログインしていない場合' do
+      #   before do
+      #     sign_out user
+      #   end
+
+      #   it 'リクエストが成功しないこと' do
+      #     get user_path user.id
+      #     expect(response.status).to eq 302
+      #   end        
+      # end
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,4 +66,5 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include FactoryBot::Syntax::Methods
+  config.include Devise::Test::ControllerHelpers, type: :controller
 end
