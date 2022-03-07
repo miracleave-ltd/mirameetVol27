4ba94a6cdf706916b3143d2a494fcf9eebaca76d
toshihiko kobayashi <toshihiko316@gmail.com>
2022-03-07 20:45:08 +0900

    ログインしていない場合の302レスポンスを削除(createアクション)

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -100,11 +100,6 @@ RSpec.describe PostsController, type: :controller do
           sign_out user
         end
 
-        it '302レスポンスを返すこと' do          
-          subject
-          expect(response.status).to eq 302
-        end
-
         it 'ログイン画面にリダイレクトされること' do          
           subject
           expect(response).to redirect_to new_user_session_url
