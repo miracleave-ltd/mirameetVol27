93605aded4b3faccbe6038808cd68d94000b5e59
toshihiko kobayashi <toshihiko316@gmail.com>
2022-03-08 22:17:16 +0900

    indexアクションのpost_instanceは不要の為、削除

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -7,8 +7,7 @@ RSpec.describe PostsController, type: :controller do
     subject { get :index }
     context 'ログインしている場合' do
       before do
-        sign_in user
-        post_instance
+        sign_in user        
       end
 
       it '200レスポンスを返すこと' do          
